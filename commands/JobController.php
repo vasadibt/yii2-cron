47f5cbe8da7a18712909aa8c0fb8db76aee94954
Sándor Gál <45456263+fullmvc@users.noreply.github.com>
2020-07-03 20:53:04 +0200

    Update JobController.php

--- commands/JobController.php
+++ commands/JobController.php
@@ -14,38 +14,36 @@ use yii\helpers\Console;
 class JobController extends Controller
 {
     /**
-     * @param $id
+     * @param int $id
      */
     public function actionRun($id)
     {
         $job = CronJob::findOne($id);
         $run = $job->run();
-        $writer = function ($message) {
-            try {
-                return Console::output($message);
-            } catch (\Exception $e) {
-                $message = $message . PHP_EOL;
-                echo $message;
-                return strlen($message);
-            }
-        };
 
-        $writer('Process is finished, exit code: #' . $run->exit_code);
-        $writer($run->output);
+        $this->output('Process is finished, exit code: #' . $run->exit_code);
+        $this->output($run->output);
 
         if ($run->error_output) {
-            $writer($run->error_output);
+            $this->output($run->error_output);
         }
     }
 
     /**
-     * @param $id
+     * @param int $id
      */
     public function actionRunQuick($id)
     {
         $job = CronJob::findOne($id);
         $job->runQuick();
-        Console::output('Job started without wait finish: ' . $job->name);
+        $this->output('Job started without wait finish: ' . $job->name);
+    }
+
+    /**
+     * @param string $message
+     */
+    protected function output($message){
+        echo $message . PHP_EOL;
     }
 }
 
